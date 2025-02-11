e4675687818eabbe9d67871b027dbd2e7667a004
YeohTH <soundwavedecepticonleader@gmail.com>
2025-02-11 22:47:06 +0800

    please work

--- src/SineWaveGenerator.jsx
+++ src/SineWaveGenerator.jsx
@@ -23,4 +23,74 @@ const SineWaveGenerator = ({ frequency, amplitude }) => {
 
             for (let i = 0; i < wave.length; i++) {
                 const x = (canvas.width * i) / wave.length;
-                const y = (canva
+                const y = (canvas.height / 2) - wave[i] * (canvas.height / 2);
+                ctx.lineTo(x, y);
+            }
+
+            ctx.strokeStyle = "#EEEEEE";
+            ctx.lineWidth = 3;
+            ctx.stroke();
+        };
+
+        const sine_wave = (amplitude, frequency, duration, sr) => {
+            const samples = duration * sr;
+            const wave = [];
+            for (let i = 0; i < samples; i++) {
+                const sample = amplitude * Math.sin((2 * Math.PI * frequency * i) / sr);
+                wave.push(sample);
+            }
+            return wave;
+        };
+
+        drawSineWave();
+
+        return () => {
+            if (source) {
+                source.stop();
+                source.disconnect();
+            }
+        };
+    }, [frequency, amplitude]);
+
+    const playSound = () => {
+        if (!audioContext) {
+            setAudioContext(new (window.AudioContext || window.webkitAudioContext)());
+        }
+
+        if (source) {
+            source.stop();
+            source.disconnect();
+        }
+
+        const duration = 100;
+        const sr = 44100;
+        const samples = duration * sr;
+        const wave = sine_wave(amplitude, frequency, duration, sr);
+        const buffer = audioContext.createBuffer(1, samples, sr);
+        const data = buffer.getChannelData(0);
+
+        for (let i = 0; i < samples; i++) {
+            data[i] = wave[i];
+        }
+
+        const newSource = audioContext.createBufferSource();
+        newSource.buffer = buffer;
+        newSource.connect(audioContext.destination);
+        newSource.start();
+        setSource(newSource);
+        setIsSource(true);
+    };
+
+    const stopSound = () => {
+        if (source) {
+            source.stop();
+            source.disconnect();
+            setSource(null);
+            setIsSource(false);
+        }
+    };
+
+    return <canvas id="graph" ref={canvasRef}></canvas>;
+};
+
+export default SineWaveGenerator;
